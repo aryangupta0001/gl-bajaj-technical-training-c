e49b38ac7f10d0876a594f2ccc12f171bca08997
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-06-05 19:32:01 +0530

    removes unused vars & refactored the code

--- neg_in_starting.c
+++ neg_in_starting.c
@@ -4,36 +4,28 @@
 
 int main()
 {
-    int A[20], N, i, j, k = 0, temp;
+    int A[20], N, i, j, temp;
 
     scanf("%d", &N);
 
     for(i = 0; i<N; i++)
         scanf("%d", &A[i]);
-    
-    i = 0;
 
-    while(i<N)
-    {   if(A[i]<0)
+    for(i = 0; i<N; i++)
+        if(A[i]<0)
         {   temp = A[i];
             j = i;
 
             while(j>0)
                 if(A[j-1] >= 0)
-                {   A[j] = A[j-1];
-                    j--;
-                }
+                    A[j--] = A[j];
 
                 else
                     break;
 
             A[j] = temp;
         }
-
-        i++;
-    }
     
     for(i = 0; i<N; i++)
         printf("%d\t", A[i]);
-
 }
